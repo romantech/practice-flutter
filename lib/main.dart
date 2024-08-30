1d0cdc463e88c2bbfbc1ab6fe1869f89ad74ba6f
ColorFilter <johan@romantech.net>
2024-08-30 15:24:44 +0900

    카드 디자인

--- lib/main.dart
+++ lib/main.dart
@@ -74,7 +74,76 @@ class MyApp extends StatelessWidget {
                     textColor: Colors.white,
                   ),
                 ],
-              )
+              ),
+              const SizedBox(height: 100),
+              Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Wallets',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontWeight: FontWeight.w600,
+                        )),
+                    Text('View All',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        )),
+                  ]),
+              const SizedBox(height: 20),
+              Container(
+                  clipBehavior: Clip.hardEdge,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF1F2123),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(30),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text('Euro',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 32,
+                                      fontWeight: FontWeight.w600,
+                                    )),
+                                const SizedBox(height: 10),
+                                Row(
+                                  children: [
+                                    const Text('6 428',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                        )),
+                                    const SizedBox(width: 5),
+                                    Text('EUR',
+                                        style: TextStyle(
+                                          color: Colors.white.withOpacity(0.8),
+                                          fontSize: 20,
+                                        )),
+                                  ],
+                                )
+                              ]),
+                          Transform.scale(
+                              scale: 2.2,
+                              child: Transform.translate(
+                                offset: const Offset(-5, 12),
+                                child: const Icon(
+                                  Icons.euro_rounded,
+                                  color: Colors.white,
+                                  size: 88,
+                                ),
+                              ))
+                        ]),
+                  )),
             ])),
       ),
     );
