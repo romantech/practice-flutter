2969ee6554218921e18a6eb0ea4dbde243c2b9ab
ColorFilter <johan@romantech.net>
2024-09-01 21:07:21 +0900

    설명 주석 추가

--- lib/widgets/currency_card.dart
+++ lib/widgets/currency_card.dart
@@ -21,9 +21,14 @@ class CurrencyCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 위젯의 위치를 특정 방향으로 이동시킬 때 사용하는 Transform.translate
+    // 부모 위젯의 크기나 레이아웃에 영향을 주지 않음
     return Transform.translate(
+      // 좌표나 거리 등을 나타내기 위해 사용하는 Offset 클래스
       offset: Offset(0, (order - 1) * _baseOffset),
       child: Container(
+          // 위젯의 잘림 방식을 정의할 때 사용하는 clipBehavior 속성
+          // Clip.hardEdge는 위젯의 경계를 기준으로 즉시 잘라내는 방식
           clipBehavior: Clip.hardEdge,
           decoration: BoxDecoration(
             color: isInverted ? Colors.white : _blackColor,
@@ -64,10 +69,13 @@ class CurrencyCard extends StatelessWidget {
                           ],
                         )
                       ]),
+                  // 자식 위젯의 크기를 확대/축소할 때 사용하는 Transform.scale
+                  // 자식 위젯의 크기만 변경하고 레이아웃에는 영향 안줌
                   Transform.scale(
                       scale: 2.2,
                       child: Transform.translate(
                         offset: const Offset(-5, 12),
+                        // 아이콘을 표시하기 위해 사용하는 Icon 위젯
                         child: Icon(
                           icon,
                           color: isInverted ? _blackColor : Colors.white,
