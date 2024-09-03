d80ae2fb63d4e04441ae996b1cc56dad470d8787
ColorFilter <johan@romantech.net>
2024-09-03 22:42:09 +0900

    pomodoro - ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
-import 'package:flutter/widgets.dart';
-import 'package:practice_flutter/screens/life_cycle.dart';
+import 'package:flutter/material.dart';
+import 'package:practice_flutter/screens/home.dart';
 
 void main() {
-  runApp(const LifeCycle());
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xffE7626C),
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+            color: Color(0xff232B55),
+          ),
+        ),
+        cardColor: const Color(0xffF4EDDB),
+      ),
+      home: const HomeScreen(),
+    );
+  }
 }
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,2 +1,3 @@
 export 'card_list.dart';
+export 'home.dart';
 export 'life_cycle.dart';
