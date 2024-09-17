1e4ae73713642d571e83170a7cbcdd7b8b4e8e4f
ColorFilter <johan@romantech.net>
2024-09-17 19:14:17 +0900

    Detail Screen 추가
    - Navigator.push
    - MaterialPageRoute
    - fullscreenDialog

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final String title, thumb, id;
+
+  const DetailScreen({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.green,
+          title: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        body: Column(
+          children: [
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 250,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 15,
+                        offset: const Offset(10, 10),
+                        color: Colors.black.withOpacity(0.5),
+                      )
+                    ],
+                  ),
+                  // 네트워크 상의 이미지를 불러올 때 Image.network 사용
+                  // 모든 네트워크 이미지는 HTTP 헤더와 관계없이 캐시됨
+                  child: Image.network(thumb, headers: const {
+                    // HTTP 요청 시 Referer 헤더 추가
+                    'Referer': 'https://comic.naver.com',
+                  }),
+                ),
+              ],
+            )
+          ],
+        ));
+  }
+}
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,3 +1,4 @@
 export 'card_list.dart';
+export 'detail_screen.dart';
 export 'life_cycle.dart';
 export 'pomodoro.dart';
